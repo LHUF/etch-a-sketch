0da6119d18f5bf45f944804288eaf455f332e978
Louis Hough <sirlouishough@gmail.com>
2020-01-13 13:14:20 +0000

    style canvas ready for tile placement

---
+++ script.js
@@ -0,0 +1,10 @@
+const gridCanvas = document.getElementById("gridCanvas");
+
+//style canvas
+gridCanvas.style.height = "700px";
+gridCanvas.style.width = "700px";
+gridCanvas.style.margin = "auto";
+gridCanvas.style.backgroundColor = "white";
+gridCanvas.style.border = "2.5px solid black";
+gridCanvas.style.display = "grid";
+
