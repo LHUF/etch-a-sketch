a10f7ef065e43211748e24024719574263e2b64d
Louis Hough <sirlouishough@gmail.com>
2020-01-15 02:31:12 +0000

    function to draw canvas and insert x amount of squares

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const gridCanvas = document.getElementById("gridCanvas");
+function generateTiles(tilesInRow) {
+    const gridCanvas = document.getElementById("gridCanvas");
+    gridCanvas.style.height = "700px";
+    gridCanvas.style.width = "700px";
+    gridCanvas.style.margin = "auto";
+    gridCanvas.style.backgroundColor = "white";
+    gridCanvas.style.border = "2.5px solid black";
+    gridCanvas.style.display = "grid";
+    gridCanvas.style.gridTemplateColumns = `repeat(${tilesInRow}, 1fr)`;
+    let totalTiles = tilesInRow * tilesInRow;
+    for (let i = 0; i < totalTiles; i++) {
+        const tile = document.createElement("div");
+        tile.classList.add("tile");
+        gridCanvas.appendChild(tile);
+    }
+}
 
-//style canvas
-gridCanvas.style.height = "700px";
-gridCanvas.style.width = "700px";
-gridCanvas.style.margin = "auto";
-gridCanvas.style.backgroundColor = "white";
-gridCanvas.style.border = "2.5px solid black";
-gridCanvas.style.display = "grid";
-
+generateTiles(7);
